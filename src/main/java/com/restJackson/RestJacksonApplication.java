7c28f8da89791bcbb8011ce7e1f0f1f9a71cd28c
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-12-01 11:55:32 +0000

    intial commit

---
+++ src/main/java/com/restJackson/RestJacksonApplication.java
@@ -0,0 +1,13 @@
+package com.restJackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJacksonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restJackson/RestJacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restJackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJacksonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
